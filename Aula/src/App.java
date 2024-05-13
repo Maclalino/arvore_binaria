b2a5724ccdd8561203ac2a2621bca2faf9ca565a
Maria Clara Marques Lino <129996963+Maclalino@users.noreply.github.com>
2024-05-13 20:21:32 -0300

    Delete Aula directory

--- Aula/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        ArrayLista minhaLista = new ArrayLista();
-       System.out.println(minhaLista.tamanho());
-        minhaLista.inserir(8);
-        System.out.println(minhaLista.tamanho());
-        System.out.println(minhaLista.numero(0));
-    }
-}
